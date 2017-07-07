c38bdd3bfa26814277ace3674b5a77f2ef218f88
Alexey Spiridonov <forspall@gmail.com>
2017-07-07 14:15:17 +0700

    add lock

--- statapp.go
+++ statapp.go
@@ -3,25 +3,19 @@ Example:
 package main
 
 import (
-	"time"
-
 	"github.com/dzen-it/statapp"
 )
 
 func main() {
 	// Runs a implementation of the server in background
-	statapp.Start(":9999")
+	statapp.Start(":9999")   // http://localhost:9999
 
 	// sets custom parameters
 	statapp.Set("param-1", 42)
-	statapp.Set("param-2", 2030)
 
 	// Increment the value of the parameter
 	statapp.Inc("param-2", 3) // Now the value is 2033
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGTERM)
-	<-sigCh
 }
 */
 package statapp
@@ -30,18 +24,23 @@ import (
 	"encoding/json"
 	"net/http"
 	"runtime"
+	"sync"
 )
 
 var (
 	server http.Server
-	params map[string]uint64
+	params = struct{
+    sync.RWMutex
+    m map[string]uint64
+  }{m: make(map[string]uint64)}
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	var mem runtime.MemStats
 	runtime.ReadMemStats(&mem)
-
-	params["mem"], params["goroute"] = mem.HeapSys, uint64(runtime.NumGoroutine())
+	params.RLock()
+	defer params.RUnlock()
+	params.m["mem"], params.m["goroute"] = mem.HeapSys, uint64(runtime.NumGoroutine())
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(params)
@@ -50,7 +49,6 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 // Start creates new implementation of a parameters storage
 // and starts a new implementation of a server
 func Start(addr string) {
-	params = make(map[string]uint64)
 	server = http.Server{
 		Addr:    addr,
 		Handler: http.HandlerFunc(rootHandler),
@@ -66,10 +64,14 @@ func start() {
 
 // Set sets the parameter associated with integer value into a storage.
 func Set(param string, val uint64) {
-	params[param] = val
+	params.RLock()
+	defer params.RUnlock()
+	params.m[param] = val
 }
 
 // Inc increments the value of the parameter
 func Inc(param string, inc int) {
-	params[param] += uint64(inc)
+	params.RLock()
+	defer params.RUnlock()
+	params.m[param] += uint64(inc)
 }
